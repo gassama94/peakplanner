abc0603890b132738d58cc9bed8f5a3764a051e4
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-25 13:12:18 +0000

    downloaded tailwind css

---
+++ peakplanner/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
